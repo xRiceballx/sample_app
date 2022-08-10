a05e2c94607a4b9d29db2ebe1e511acaaae23dae
EC2 Default User <ec2-user@ip-172-31-28-38.ec2.internal>
2022-08-10 13:36:29 +0000

    [Finish] 3-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,11 +24,18 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
 
   private
   #ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   # .../lists/1 や .../lists/3 に該当する
   get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 end
