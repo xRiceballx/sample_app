5379b597a9998079a88a5304198125e1d5750528
EC2 Default User <ec2-user@ip-172-31-28-38.ec2.internal>
2022-08-10 10:37:57 +0000

    [Finish] 3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,8 +10,9 @@ class ListsController < ApplicationController
     list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    # 4. redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -19,6 +20,7 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
